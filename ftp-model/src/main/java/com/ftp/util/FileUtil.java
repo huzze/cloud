8a3a2cf885721e0bfbd3d9c668eb3c9f0c0b9d9a
linky <442306488@qq.com>
2024-04-07 15:40:08 +0800

    add: 添加判断url对应的文件是否存在

--- ftp-model/src/main/java/com/ftp/util/FileUtil.java
+++ ftp-model/src/main/java/com/ftp/util/FileUtil.java
@@ -2,6 +2,8 @@ package com.ftp.util;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -35,5 +37,30 @@ public class FileUtil {
         String[] half = Arrays.stream(halfWidthSymbols).filter(s -> !StringUtils.equals(s, ".")).toArray(String[]::new);
         String[] full = Arrays.stream(fullWidthSymbols).filter(s -> !StringUtils.equals(s, "．")).toArray(String[]::new);
         return StringUtils.replaceEach(name, half, full);
+    }
+
+    /**
+     * url对应的文件是否存在
+     * @param fileUrl
+     * @return
+     */
+    public static boolean isRemoteFileAvailable(String fileUrl)  {
+        HttpURLConnection connection = null;
+        try {
+            URL url = new URL(fileUrl);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("HEAD");
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+
+            int responseCode = connection.getResponseCode();
+            return responseCode >= 200 && responseCode < 300;
+        } catch (Exception e) {
+            return false;
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
     }
 }
